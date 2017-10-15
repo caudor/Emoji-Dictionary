86cc95acf2ea607c23043334e99df07cffa052e8
Christopher Richardson <chris409@live.com>
2017-10-15 17:03:56 -0500

    Fixed.  Working tableview with same value in each cell.

--- Emoji Dictionary/ImojiTableViewController.swift
+++ Emoji Dictionary/ImojiTableViewController.swift
@@ -16,15 +16,15 @@ class ImojiTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        return 20
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = "Hello there"
 
         return cell
     }
